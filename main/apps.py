3b2632567c1a3ff8cf69cc962d6ee4cee9c5c8e8
Sasha1505-ctrl <62772109+Sasha1505-ctrl@users.noreply.github.com>
2021-11-09 14:04:19 +0300

    update apply.py

--- main/apps.py
+++ main/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ClasshomeConfig(AppConfig):
+class mainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ClassHome'
+    name = 'main'
