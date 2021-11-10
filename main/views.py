35772e53a4a6a6cbeb329469ce7a01f8205e6e2e
Sasha1505-ctrl <62772109+Sasha1505-ctrl@users.noreply.github.com>
2021-11-10 09:53:56 +0300

    update views.py

--- main/views.py
+++ main/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def chat(request):
-    return render(request, 'room.html')
+    return render(request, 'chat.html', {})
+
+
+def room(request, room_name):
+    return render(request, 'room.html', {
+        'room_name': room_name
+    })
 
 
 def index(request):
