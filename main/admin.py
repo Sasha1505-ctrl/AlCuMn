7abccacf74939ea792fda5be47c599fa9225c254
Sasha1505-ctrl <62772109+Sasha1505-ctrl@users.noreply.github.com>
2021-11-10 09:47:05 +0300

    update admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Message)
+admin.site.register(Discipline)
+admin.site.register(Lesson)
+admin.site.register(User)
+admin.site.register(Certificate)
